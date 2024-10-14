ecf59e5e58d9b7bb27bd558574c74779c700a33a
Dima <d.kozhemyako@icloud.com>
2024-10-14 12:05:11 +0300

    2.3 (1) lab

--- labs/2.3/main.cpp
+++ labs/2.3/main.cpp
@@ -2,83 +2,115 @@
  * Done by:
  * Student Name: Dima Kozhemiako
  * Student Group: 123
- * Lab 2.2
+ * Lab 2.3
  */
 
 #include <iostream>
 using namespace std;
 
+const int MAX_SIZE = 10;
+
+// Структура для стека
+struct Stack {
+    double data[MAX_SIZE]; // масив для зберігання 
+    int top; // індекс стека
+
+    // Конструктор 
+    Stack() {
+        top = -1; // стек порожній
+    }
+
+    // Метод для додавання елемента
+    void push(double value) {
+        if (top < MAX_SIZE - 1) {
+            data[++top] = value; // зберігаємо елемент
+        }
+        else {
+            cout << "Стек переповнений!" << endl;
+        }
+    }
+
+    // Метод для видалення елемента
+    double pop() {
+        if (top >= 0) {
+            return data[top--]; // повертаємо верхній елемент
+        }
+        else {
+            cout << "Стек порожній!" << endl;
+            return -1; // позначення помилки
+        }
+    }
+};
+
+// Структура для вузла односпрямованого списку
+struct Node {
+    int data; // дані
+    Node* next; // вказівник на наступний елемент
+};
+
+// Структура для односпрямованого списку
+struct LinkedList {
+    Node* head; // вказівник на перший елемент
+
+    // Конструктор для ініціалізації списку
+    LinkedList() {
+        head = nullptr; // список порожній
+    }
+
+    // Метод для додавання елемента в кінець списку
+    void append(int value) {
+        Node* newNode = new Node(); // створюємо новий елемент
+        newNode->data = value; // заповнюємо дані
+        newNode->next = nullptr; // новий елемент вказує на нуль
+
+        if (head == nullptr) {
+            head = newNode; // якщо список порожній, новий елемент стає першим
+        }
+        else {
+            Node* temp = head;
+            while (temp->next != nullptr) {
+                temp = temp->next; // переходимо до кінця 
+            }
+            temp->next = newNode; // додаємо новий 
+        }
+    }
+
+    // Метод для видалення всіх елементів списку
+    void clear() {
+        Node* temp;
+        while (head != nullptr) {
+            temp = head; // запам'ятовуємо 
+            head = head->next; // до наступного
+            delete temp; // видаляємо старий 
+        }
+    }
+};
+
 int main() {
-    enum Names { Dima, Natasha, Sergey, Kate, Kira, Roman, Max };
+    // Приклад стека
+    Stack stack;
+    stack.push(5.5);
+    stack.push(10.0);
+    cout << "Зняти зі стека: " << stack.pop() << endl;
 
-    //v4.1
-    struct Student
-    {
-        Names name;
-        float averageScore;
-        bool dancing;
-    };
+    // Приклад списку
+    LinkedList list;
+    list.append(1);
+    list.append(2);
+    list.append(3);
+    cout << "Елементи односпрямованого списку: ";
 
-    Student myStudents[7];
-
-    myStudents[0] = { Dima, 1, true };
-    myStudents[1] = { Natasha, 2, true };
-    myStudents[2] = { Sergey, 3, false };
-    myStudents[3] = { Kate, 4, false };
-    myStudents[4] = { Kira, 5, true };
-    myStudents[5] = { Roman, 5, true };
-    myStudents[6].name = Max;
-    myStudents[6].averageScore = 4;
-    myStudents[6].dancing = true;
-
-    int count = 0;
-
-    for (int i = 0; i < 7; i++)
-
-    {
-        if (myStudents[i].averageScore == 5 && myStudents[i].dancing == true)
-        {
-            count++;
-        }
+    // Виведення 
+    Node* temp = list.head;
+    while (temp != nullptr) {
+        cout << temp->data << " ";
+        temp = temp->next; // до наступного елемента
     }
-    float percent = (float)count * 100 / 7;
+    cout << endl;
 
-    cout << "v4.1^: " << percent << "%" << endl;
-
-    //v4.2
-    struct Student2
-    {
-        Names name;
-        float course;
-        bool army;
-    };
-
-    Student2 myStudents2[7];
-
-    myStudents2[0] = { Dima, 1, true };
-    myStudents2[1] = { Natasha, 2, true };
-    myStudents2[2] = { Sergey, 3, false };
-    myStudents2[3] = { Kate, 2, false };
-    myStudents2[4] = { Kira, 5, true };
-    myStudents2[5] = { Roman, 5, true };
-    myStudents2[6].name = Max;
-    myStudents2[6].course = 4;
-    myStudents2[6].army = true;
-
-    int count2 = 0;
-
-    for (int i = 0; i < 7; i++)
-
-    {
-        if (myStudents2[i].course == 2 && myStudents2[i].army == false)
-        {
-            count2++;
-        }
-    }
-    float percent2 = (float)count2 * 100 / 7;
-
-    cout << "v4.2^: " << percent2 << "%";
-
+    // Очистка 
+    list.clear();
 
     return 0;
 }
+
