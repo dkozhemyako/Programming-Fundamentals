8d61b214880626269a8134ee0090f93fbbf72295
dk <d.kozhemyako@icloud.com>
2024-10-15 21:57:57 +0300

    correct rgr

--- rgr/main.cpp
+++ rgr/main.cpp
@@ -2,115 +2,68 @@
  * Done by:
  * Student Name: Dima Kozhemiako
  * Student Group: 123
- * Lab 2.3
+ * RGR
  */
 
 #include <iostream>
+#include <cstring> 
+
 using namespace std;
 
-const int MAX_SIZE = 10;
+bool isDigit(char ch) {
+    return ch >= '0' && ch <= '9';
+}
 
-// Структура для стека
-struct Stack {
-    double data[MAX_SIZE]; // масив для зберігання 
-    int top; // індекс стека
+bool isLetter(char ch) {
+    return ch >= 'P' && ch <= 'Z';
+}
 
-    // Конструктор 
-    Stack() {
-        top = -1; // стек порожній
+bool isValidChain(char chain[]) {
+
+    if (chain[0] != '+') {
+        return false;
     }
 
-    // Метод для додавання елемента
-    void push(double value) {
-        if (top < MAX_SIZE - 1) {
-            data[++top] = value; // зберігаємо елемент
-        }
-        else {
-            cout << "Стек переповнений!" << endl;
-        }
+    int i = 1; 
+
+    while (chain[i] != '\0' && isDigit(chain[i])) {
+        i++;
     }
 
-    // Метод для видалення елемента
-    double pop() {
-        if (top >= 0) {
-            return data[top--]; // повертаємо верхній елемент
-        }
-        else {
-            cout << "Стек порожній!" << endl;
-            return -1; // позначення помилки
-        }
-    }
-};
-
-// Структура для вузла односпрямованого списку
-struct Node {
-    int data; // дані
-    Node* next; // вказівник на наступний елемент
-};
-
-// Структура для односпрямованого списку
-struct LinkedList {
-    Node* head; // вказівник на перший елемент
-
-    // Конструктор для ініціалізації списку
-    LinkedList() {
-        head = nullptr; // список порожній
+    if (i == 1) {
+        return false;
     }
 
-    // Метод для додавання елемента в кінець списку
-    void append(int value) {
-        Node* newNode = new Node(); // створюємо новий елемент
-        newNode->data = value; // заповнюємо дані
-        newNode->next = nullptr; // новий елемент вказує на нуль
-
-        if (head == nullptr) {
-            head = newNode; // якщо список порожній, новий елемент стає першим
+    if (chain[i] != '\0' && isLetter(chain[i])) {
+        while (chain[i] != '\0' && isLetter(chain[i])) {
+            i++;
         }
-        else {
-            Node* temp = head;
-            while (temp->next != nullptr) {
-                temp = temp->next; // переходимо до кінця 
-            }
-            temp->next = newNode; // додаємо новий 
-        }
+        return chain[i] == '\0';
     }
 
-    // Метод для видалення всіх елементів списку
-    void clear() {
-        Node* temp;
-        while (head != nullptr) {
-            temp = head; // запам'ятовуємо 
-            head = head->next; // до наступного
-            delete temp; // видаляємо старий 
-        }
-    }
-};
+    return false;
+}
 
 int main() {
-    // Приклад стека
-    Stack stack;
-    stack.push(5.5);
-    stack.push(10.0);
-    cout << "Зняти зі стека: " << stack.pop() << endl;
+   
+    const char* chains[] = {
+        "+7888SRT",   
+        "+SRT",        
+        "+7888"        
+    };
 
-    // Приклад списку
-    LinkedList list;
-    list.append(1);
-    list.append(2);
-    list.append(3);
-    cout << "Елементи односпрямованого списку: ";
-
-    // Виведення 
-    Node* temp = list.head;
-    while (temp != nullptr) {
-        cout << temp->data << " ";
-        temp = temp->next; // до наступного елемента
+    for (int i = 0; i < 3; i++) {
+        cout << "Chain: " << chains[i] << " - ";
+        if (isValidChain(const_cast<char*>(chains[i]))) {
+            cout << "valid" << endl;
+        }
+        else {
+            cout << "not valid" << endl;
+        }
     }
-    cout << endl;
-
-    // Очистка 
-    list.clear();
 
     return 0;
 }
+
+
 
